e30f8cc27e647e540964e0bdab1e1ba4a27df0e4
legop3 <minecratfd8@gmail.com>
2022-10-01 23:19:33 -0400

    still trying to add socketio
    Co-authored-by: in5dev <in5dev@users.noreply.github.com>

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const { JsonDB, Config } = require('node-json-db');
-var cron = require('node-cron');
+// var cron = require('node-cron');
 
 
 var db = new JsonDB(new Config("pagedb", true, true, '/'))
@@ -20,24 +20,41 @@ server.listen(80, () => {
     console.log('listening on 80')
 })
 
-cron.schedule('* * * * *', () => {
-    socket.emit('counter', counter)
-    console.log('updating counter')
-})
+// cron.schedule('* * * * *', () => {
+//     socket.emit('counter', counter)
+//     console.log('updating counter')
+// })
 
 io.on('connection', (socket) => {
-    console.log(socket)
-    console.log('a user connected');
-    (async () => {
-        counter = await db.getData('/counter/')
-        db.push('/counter/', counter + 1)
-        console.log(counter)
-    })();
 
 
 
+    io.emit("join", socket.id)
+
+    socket.on("disconnect", (reason) => {
+        console.log(`user ${socket.id} disconnected`)
+        socket.broadcast.emit("leave", socket.id)
+        socket.removeAllListeners();
+    })
+    // console.log(socket)
+    console.log(`user ${socket.id} connected`);
+    // socket.broadcast.emit('counter', counter)
+    // socket.emit("hello", 1, "2", { 3: Buffer.from([4])})
+    // io.emit('counter', counter)
+    // (async () => {
+    //     counter = await db.getData('/counter/')
+    //     db.push('/counter/', counter + 1)
+    //     console.log(counter)
+    // })();
+    socket.on("connect_error", (err) => {
+        console.log(`connect_error due to ${err.message}`);
+      });
+
+
+    
 
 
 });
+
 
 
